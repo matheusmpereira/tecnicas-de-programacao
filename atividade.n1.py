c14e3c50b483673969b1355a28552f72e593308f
Davi Lopes da Silva <90329931+davi7295@users.noreply.github.com>
2021-09-13 12:33:22 -0300

    Laço de repetição
    Criando o inicio do laço de repetição para a primeira jogada com as suas instruções...

--- atividade.n1.py
+++ atividade.n1.py
@@ -3,3 +3,21 @@ movimentos= 0
 casa = 1
 venceu = 0
 jogada= 0
+
+while venceu == 0:
+	if movimentos >= 6:
+		print("Você atingiu o limite de movimentos!")
+		break;
+	else:
+		while casa == 1 and movimentos <7:
+			print("Você esta na casa: ",casa)
+			print("Você ainda tem", 7-movimentos,"movimentos")
+			print("Escolha seu caminho:")
+			print("[1]- Caminho vermelho")
+			print("[2]- Caminho preto")
+			jogada = int(input(""))
+			print("")
+			if jogada == 1:
+				casa = 2
+				movimentos = movimentos+1
+				break;
