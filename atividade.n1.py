acbb427e11e26719b1363b9b5cd04e644810b0f8
MateusCPA <89863752+MateusCPA@users.noreply.github.com>
2021-09-14 15:44:26 -0300

    Update atividade.n1.py
    While principal (while pai)

--- atividade.n1.py
+++ atividade.n1.py
@@ -3,3 +3,12 @@ movimentos= 0
 casa = 1
 venceu = 0
 jogada= 0
+while venceu == 0:
+	while jogadas< 7:
+		print("Voce esta na sala:",sala)
+		print("jogadas restantes:",7-jogadas)
+		print("Escolha seu caminho:")
+		print("[1] - Caminho vermelho")
+		print("[2] - Caminho preto")
+		escolha = int(input(""))
+		break;
